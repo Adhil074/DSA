4be8d149380d6e279e4de9fa6de1eb9070ecf744
Adhil074 <adhil5781@gmail.com>
2026-08-12 12:59:39 +0530

    Time: 11 ms (53.17%) | Memory: 64.7 MB (71.71%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.js
+++ 860-design-circular-queue/design-circular-queue.js
@@ -2,32 +2,31 @@
  * @param {number} k
  */
 var MyCircularQueue = function (k) {
-  this.capacity = k; 
-  this.arr = new Array(k); 
-  this.size = 0; 
-  this.f = 0; 
-  this.r = -1; 
+    this.capacity = k;
+    this.arr = new Array(k);
+    this.size = 0;
+    this.f = 0;
+    this.r = -1;
 };
 
 /** 
  * @param {number} value
  * @return {boolean}
  */
-    MyCircularQueue.prototype.enQueue = function (value) {
-  if (this.size === this.capacity) return false;
-  this.r = (this.r + 1) % this.capacity;
+MyCircularQueue.prototype.enQueue = function (value) {
+  if (this.isFull()) return false;
+  this.r = (this.r + 1) % this.capacity; // Fixed: this.r
   this.arr[this.r] = value;
   this.size++;
   return true;
 };
 
-
 /**
  * @return {boolean}
  */
 MyCircularQueue.prototype.deQueue = function () {
-  if (this.size === 0) return false;
-  this.f = (this.f + 1) % this.capacity;
+  if (this.isEmpty()) return false;
+  this.f = (this.f + 1) % this.capacity; // Fixed: this.f
   this.size--;
   return true;
 };
@@ -36,7 +35,7 @@ MyCircularQueue.prototype.deQueue = function () {
  * @return {number}
  */
 MyCircularQueue.prototype.Front = function () {
-  if (this.size === 0) return -1;
+  if (this.isEmpty()) return -1; // Fixed: returns -1
   return this.arr[this.f];
 };
 
@@ -44,26 +43,22 @@ MyCircularQueue.prototype.Front = function () {
  * @return {number}
  */
 MyCircularQueue.prototype.Rear = function () {
-  if (this.size === 0) return -1;
+  if (this.isEmpty()) return -1; // Fixed: returns -1
   return this.arr[this.r];
 };
-
 /**
  * @return {boolean}
  */
 MyCircularQueue.prototype.isEmpty = function () {
-  if (this.size === 0) return true;
-  return false;
+  return this.size === 0; // Fixed: true if size is 0
 };
 
 /**
  * @return {boolean}
  */
 MyCircularQueue.prototype.isFull = function () {
-  if (this.size === this.capacity) return true;
-  return false;
+  return this.size === this.capacity; // Fixed: true if size equals capacity
 };
-
 /** 
  * Your MyCircularQueue object will be instantiated and called as such:
  * var obj = new MyCircularQueue(k)
