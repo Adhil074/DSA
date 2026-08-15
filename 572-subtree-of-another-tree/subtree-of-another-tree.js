88c257d02f3d2b180acc1584ed238ef3b9ba1d8e
Adhil074 <adhil5781@gmail.com>
2026-08-15 10:19:44 +0530

    Time: 9 ms (28.90%) | Memory: 62.3 MB (61.53%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+
+var isSubtree = function (root, subRoot) {
+    if (root === null) return false;
+    let isSameTree = function (root1, root2) {
+        if (root1 === null && root2 === null) return true;
+        if (root1 === null || root2 === null) return false;
+        if (root1.val !== root2.val) return false;
+        return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
+    };
+    if (isSameTree(root, subRoot)) return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
