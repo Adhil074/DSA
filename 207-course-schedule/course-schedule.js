80f1f441a7c87b03a387f7f10513ced9d56a9284
Adhil074 <adhil5781@gmail.com>
2026-08-28 19:10:44 +0530

    Time: 15 ms (41.79%) | Memory: 62.1 MB (59.29%) - LeetSync

--- 207-course-schedule/course-schedule.js
+++ 207-course-schedule/course-schedule.js
@@ -5,25 +5,24 @@
  */
 var canFinish = function (numCourses, prerequisites) {
     let adj = Array.from({ length: numCourses }, () => []);
-    for (let [u, v] of prerequisites) adj[v].push(u);
-    let visited = new Array(numCourses).fill(false);
-    let pathVisited = new Array(numCourses).fill(false);
-    function dfs(node) {//node: Tells DFS which specific course it is currently inspecting and checking for cycles.
-        // 1. Cycle found -> return true
-        if (pathVisited[node] === true) return true;
-        // 2. Already processed and safe -> return false
-        if (visited[node] === true) return false;
-        pathVisited[node] = true;
-        visited[node] = true;
+    let indegree = new Array(numCourses).fill(0);
+    for (let [u, v] of prerequisites) {
+        adj[v].push(u);
+        indegree[u]++;
+    }
+    let q = [];
+    let res = [];
+    for (let i = 0; i < indegree.length; i++) {
+        if (indegree[i] == 0) q.push(i);
+    }
+    while (q.length > 0) {
+        let node = q.shift();
+        res.push(node);
         for (let neighbor of adj[node]) {
-            if (dfs(neighbor) === true) return true; // Found a cycle! Stop and return true immediately.
+            indegree[neighbor]--;
+            if (indegree[neighbor] === 0) q.push(neighbor);
         }
-        // Only AFTER checking ALL neighbors safely, backtrack and return false
-        pathVisited[node] = false;
-        return false; // No cycle found in any branch
     }
-    for (let i = 0; i < numCourses; i++) {
-        if (dfs(i) === true) return false; // Cycle found! Impossible to finish.
-    }
-    return true; // Checked all courses without any cycles!
+    if (res.length === numCourses) return true;
+    return false;
 };
